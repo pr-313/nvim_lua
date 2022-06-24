ddd58060f0c9a80ef828edecc7a11f13fb85d71f
PRATYUSH RAVINDRA BHARATI <pbharati@micron.com>
2022-06-24 09:39:42 +0530

    more changes ported over

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -14,6 +14,43 @@ local servers = {
   "yamlls",
 }
 
+
+lsp_installer.settings({
+ ui = {
+        keymaps = {
+            -- Keymap to expand a server in the UI
+            toggle_server_expand = "<CR>",
+            -- Keymap to install a server
+            install_server = "i",
+            -- Keymap to reinstall/update a server
+            update_server = "u",
+            -- Keymap to update all installed servers
+            update_all_servers = "U",
+            -- Keymap to uninstall a server
+            uninstall_server = "X",
+        },
+    },
+
+    -- The directory in which to install all servers.
+    install_root_dir = "/runs/simrun_tav/libs/lsp_server",
+
+    pip = {
+        -- These args will be added to `pip install` calls. Note that setting extra args might impact intended behavior
+        -- and is not recommended.
+        --
+        -- Example: { "--proxy", "https://proxyserver" }
+        install_args = {},
+    },
+
+    -- Controls to which degree logs are written to the log file. It's useful to set this to vim.log.levels.DEBUG when
+    -- debugging issues with server installations.
+    log_level = vim.log.levels.INFO,
+
+    -- Limit for the maximum amount of servers to be installed at the same time. Once this limit is reached, any further
+    -- servers that are requested to be installed will be put in a queue.
+    max_concurrent_installers = 4,
+})
+
 lsp_installer.setup()
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
