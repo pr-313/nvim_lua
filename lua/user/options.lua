775ca884e8e6f4928a10ef5c242fcfc0baa3a15b
PRATYUSH RAVINDRA BHARATI <pbharati@micron.com>
2022-07-05 17:28:47 +0530

    added shift select features

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -34,6 +34,7 @@ vim.opt.signcolumn = "yes"                      -- always show the sign column, 
 vim.opt.wrap = false                            -- display lines as one long line
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
+vim.opt.keymodel= {"startsel","stopsel"}
 -- vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
 vim.opt.fillchars.eob=" "
 vim.opt.shortmess:append "c"
