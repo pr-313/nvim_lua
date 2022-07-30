f295dd095c2a51358d266b6dc60f809bdb167dfb
Pratyush Bharati <pbharati@Pratyushs-MacBook-Pro.local>
2022-07-30 21:05:34 +0530

    more colorssss

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,8 @@
-local colorscheme = "tokyonight"
+
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+vim.g.gruvbox_contrast_dark = "hard"
+local colorscheme = "gruvbox"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
