00106d9feb576f957961e25f1b807d2ac62b2c2f
PRATYUSH RAVINDRA BHARATI <pbharati@micron.com>
2022-08-01 18:17:53 +0530

    fix sessions

--- lua/user/sessions.lua
+++ lua/user/sessions.lua
@@ -8,8 +8,8 @@ local opts = {
   auto_session_enable_last_session = false,
   auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
   auto_session_enabled = true,
-  auto_save_enabled = nil,
-  auto_restore_enabled = nil,
+  auto_save_enabled = true,
+  auto_restore_enabled = false,
   auto_session_suppress_dirs = nil,
   auto_session_use_git_branch = true,
   -- the configs below are lua only
