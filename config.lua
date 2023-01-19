650bbe00a20fa1f9c4e922c5e5b2e7036f873bd1
Pratyush Bharati <pbharati@Pratyushs-MacBook-Pro.local>
2023-01-19 12:40:41 +0530

    updates

--- config.lua
+++ config.lua
@@ -34,7 +34,7 @@ lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 -- -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["W"] = { "<cmd>noautocmd w<cr>", "Save without formatting" }
+lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope live_grep<cr>", "Live Grep" }
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 
 -- -- Change theme settings
@@ -44,13 +44,20 @@ lvim.builtin.illuminate.active = false
 lvim.builtin.lir.active = false
 lvim.builtin.project.active = false
 lvim.builtin.dap.active = false
+lvim.builtin.bufferline.active = false
 
 lvim.builtin.alpha.dashboard.section.buttons.entries[1][3] = "<CMD>Telescope git_files<CR>"
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
+
 lvim.builtin.terminal.active = true
+
 lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.diagnostics.enable = false
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.actions.change_dir.global = true
+lvim.builtin.nvimtree.setup.update_focused_file.enable = false
+lvim.builtin.nvimtree.setup.update_focused_file.update_root = false
 
 -- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = false
