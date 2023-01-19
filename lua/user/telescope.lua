866cc5f63b8666db123490f74c3efa2370cbf229
Pratyush Bharati <pbharati@Pratyushs-MacBook-Pro.local>
2023-01-19 16:20:50 +0530

    telescope layout fix

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -3,11 +3,29 @@ function Get_search_seed_from_prompt()
   require('telescope.builtin').grep_string({ search = vim.fn.input("Grep > ") });
 end
 
-lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope live_grep<cr>", "Live Grep" }
-lvim.builtin.which_key.mappings["sl"] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Current Buffer" }
-lvim.builtin.which_key.mappings["F"] = { ":lua Get_search_seed_from_prompt()<CR>", "Fuzzy Grep with Seed Word" }
 function Find_selection()
   require('telescope.builtin').grep_string({ search = vim.fn.getreg('"') });
 end
 
+vim.keymap.set("n", "te", ":Telescope<CR>", { noremap = true, silent = true })
+lvim.builtin.which_key.mappings["f"] = { "<cmd>Telescope live_grep<cr>", "Live Grep" }
+lvim.builtin.which_key.mappings["sl"] = { "<cmd>Telescope current_buffer_fuzzy_find<cr>", "Current Buffer" }
+lvim.builtin.which_key.mappings["F"] = { ":lua Get_search_seed_from_prompt()<CR>", "Fuzzy Grep with Seed Word" }
+
 lvim.builtin.which_key.vmappings["f"] = { "y:lua Find_selection()<cr>", "Fuzzy Grep with Seed Word" }
+lvim.builtin.telescope.theme = "ivy"
+lvim.builtin.telescope.defaults.layout_strategy = 'flex'
+lvim.builtin.telescope.defaults.layout_config = {
+  height = 0.90,
+  width = 0.90,
+  prompt_position = 'bottom',
+  flex = {
+    flip_columns = 80,
+  },
+  horizontal = {
+    preview_width = 50
+  },
+  vertical = {
+    preview_height = 30
+  }
+}
