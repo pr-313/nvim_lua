56c59e2bbf4f9cf09c3108c9734dcbfca8880e46
Pratyush Bharati <pbharati@Pratyushs-MacBook-Pro.local>
2023-01-19 10:26:53 +0530

    cleanup

--- config.lua
+++ config.lua
@@ -40,6 +40,11 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- -- Change theme settings
 lvim.colorscheme = "tokyonight-night"
 
+lvim.builtin.illuminate.active = false
+lvim.builtin.lir.active = false
+lvim.builtin.project.active = false
+lvim.builtin.dap.active = false
+
 lvim.builtin.alpha.dashboard.section.buttons.entries[1][3] = "<CMD>Telescope git_files<CR>"
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
