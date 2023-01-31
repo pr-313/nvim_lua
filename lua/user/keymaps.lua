66f60577d0d9a5a30c65041d1eea588cb1d4942a
pr-313 <bharati.pratyush@gmail.com>
2023-01-31 06:40:18 +0530

    minor update

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -77,14 +77,14 @@ lvim.builtin.which_key.mappings["W"] = { ":set wrap! wrap?<CR>", "Toggle Wrap" }
 lvim.builtin.which_key.mappings["/"] = {}
 lvim.builtin.which_key.mappings["c"] = {
   name = "Comment",
-  c = {"<Plug>(comment_toggle_linewise_current)", "Comment toggle current line"},
-  b = {"<Plug>(comment_toggle_blockwise_current)", "Comment toggle block"}
+  c = { "<Plug>(comment_toggle_linewise_current)", "Comment toggle current line" },
+  b = { "<Plug>(comment_toggle_blockwise_current)", "Comment toggle block" }
 }
 
 lvim.builtin.which_key.vmappings["c"] = {
   name = "Comment",
-  c = {"<Plug>(comment_toggle_linewise_current)", "Comment toggle current line"},
-  b = {"<Plug>(comment_toggle_blockwise_current)", "Comment toggle block"}
+  c = { "<Plug>(comment_toggle_linewise_current)", "Comment toggle current line" },
+  b = { "<Plug>(comment_toggle_blockwise_current)", "Comment toggle block" }
 }
 
 -- Close buffers
