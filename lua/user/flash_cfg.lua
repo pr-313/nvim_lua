f1569ac8d59519bab765d2822daf19154991ef92
pr-313 <bharati.pratyush@gmail.com>
2023-07-12 20:40:19 -0600

    no thanks I like tts

--- lua/user/flash_cfg.lua
+++ lua/user/flash_cfg.lua
@@ -150,7 +150,7 @@ return {
         -- options used when flash is activated through
         -- `f`, `F`, `t`, `T`, `;` and `,` motions
         char = {
-            enabled = true,
+            enabled = false,
             -- dynamic configuration for ftFT motions
             config = function(opts)
                 -- autohide flash when in operator-pending mode
