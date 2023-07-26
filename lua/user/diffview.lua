7b4f46ebdb3b559942f77c955e60f76c2c7f67d9
pr-313 <bharati.pratyush@gmail.com>
2023-07-26 22:29:20 +0530

    merge with micron stuff

--- lua/user/diffview.lua
+++ lua/user/diffview.lua
@@ -5,44 +5,34 @@ end
 local actions = require("diffview.actions")
 
 diffview.setup({
-  diff_binaries = false,    -- Show diffs for binaries
-  enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
-  git_cmd = { "git" },      -- The git executable followed by default args.
-  use_icons = true,         -- Requires nvim-web-devicons
-  icons = {                 -- Only applies when use_icons is true.
-    folder_closed = "",
-    folder_open = "",
-  },
-  signs = {
-    fold_closed = "",
-    fold_open = "",
-  },
-  file_panel = {
-    listing_style = "tree",             -- One of 'list' or 'tree'
-    tree_options = {                    -- Only applies when listing_style is 'tree'
-      flatten_dirs = true,              -- Flatten dirs that only contain one single dir
-      folder_statuses = "only_folded",  -- One of 'never', 'only_folded' or 'always'.
+    diff_binaries = false,  -- Show diffs for binaries
+    enhanced_diff_hl = false, -- See ':h diffview-config-enhanced_diff_hl'
+    git_cmd = { "git" },    -- The git executable followed by default args.
+    use_icons = true,       -- Requires nvim-web-devicons
+    icons = {               -- Only applies when use_icons is true.
+        folder_closed = "",
+        folder_open = "",
     },
     signs = {
         fold_closed = "",
         fold_open = "",
     },
     file_panel = {
-        listing_style = "tree",        -- One of 'list' or 'tree'
+        listing_style = "tree", -- One of 'list' or 'tree'
         tree_options = {
-                                       -- Only applies when listing_style is 'tree'
+            -- Only applies when listing_style is 'tree'
             flatten_dirs = true,       -- Flatten dirs that only contain one single dir
             folder_statuses = "only_folded", -- One of 'never', 'only_folded' or 'always'.
         },
         win_config = {
-                                       -- See ':h diffview-config-win_config'
+            -- See ':h diffview-config-win_config'
             position = "left",
             width = 35,
         },
     },
     file_history_panel = {
         log_options = {
-                    -- See ':h diffview-config-log_options'
+            -- See ':h diffview-config-log_options'
             git = {
                 single_file = {
                     diff_merges = "combined",
@@ -53,7 +43,7 @@ diffview.setup({
             },
         },
         win_config = {
-                   -- See ':h diffview-config-win_config'
+            -- See ':h diffview-config-win_config'
             position = "bottom",
             height = 16,
         },
@@ -62,7 +52,7 @@ diffview.setup({
         win_config = {}, -- See ':h diffview-config-win_config'
     },
     default_args = {
-                     -- Default args prepended to the arg-list for the listed commands
+        -- Default args prepended to the arg-list for the listed commands
         DiffviewOpen = {},
         DiffviewFileHistory = {},
     },
