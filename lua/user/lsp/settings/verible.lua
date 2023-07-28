44527509a226acd4cb94bfe9ee34095128e42b53
pbharati <pbharati@micron.com>
2023-07-28 14:16:56 +0530

    plugs bump, verible line length inc

--- lua/user/lsp/settings/verible.lua
+++ lua/user/lsp/settings/verible.lua
@@ -1,8 +1,8 @@
 return {
     filetypes = { "systemverilog", "verilog", "verilog_systemverilog" },
     cmd = { "verible-verilog-ls",
-            "--rules=+endif-comment,+line-length=length:160,-no-trailing-spaces,-plusarg-assignment",
-            "--column_limit", "160",
+            "--rules=+endif-comment,+line-length=length:260,-no-trailing-spaces,-plusarg-assignment",
+            "--column_limit", "260",
             "--indentation_spaces", "4"
         }
 }
