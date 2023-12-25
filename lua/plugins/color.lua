2fc5f31cf61af70859ddbe467f8287d962e8a3c8
pbharati <bharati.pratyush@gmail.com>
2023-12-26 00:10:33 +0530

    fixing some basic shit

---
+++ lua/plugins/color.lua
@@ -0,0 +1,23 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+
+}
---
+++ lua/plugins/disabled_plugs.lua
@@ -0,0 +1,14 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- disable trouble
+  { "folke/flash.nvim", enabled = false },
+
+}
