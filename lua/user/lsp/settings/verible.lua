4b957115c85191a58a2c84e606d4e75a383f5495
pbharati <pbharati@micron.com>
2024-03-18 17:22:55 +0530

    verible change again

--- lua/user/lsp/settings/verible.lua
+++ lua/user/lsp/settings/verible.lua
@@ -1,8 +1,8 @@
 return {
     filetypes = { "systemverilog", "verilog", "verilog_systemverilog" },
     cmd = { "verible-verilog-ls",
-            "--rules=+endif-comment,+line-length=length:140,-no-trailing-spaces,-plusarg-assignment",
-            "--column_limit", "140",
+            "--rules=+endif-comment,+line-length=length:200,-no-trailing-spaces,-plusarg-assignment",
+            "--column_limit", "200",
             "--indentation_spaces", "4"
         }
 }
