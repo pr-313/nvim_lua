79543be718532b69789a123b65a9fcb4459b039c
pbharati <bharati.pratyush@gmail.com>
2025-10-28 20:29:51 +0530

    more

--- lua/plugins/personal.lua
+++ lua/plugins/personal.lua
@@ -9,4 +9,23 @@ return {
       colorscheme = "gruvbox",
     },
   },
+
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
+
+  {
+    "snacks.nvim",
+    opts = {
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false }, -- we set this in options.lua
+      toggle = { map = LazyVim.safe_keymap_set },
+      words = { enabled = false },
+    },
+  },
 }
